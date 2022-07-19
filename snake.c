523de99ef2403076170e080af08d5a6842abba15
Sam Barnes <sambarnesdev@icloud.com>
2022-07-19 12:23:47 -0700

    Added documentation and fixed potential bug of screen full of apples causing infinite loop

--- snake.c
+++ snake.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define DELAY 75000
+#define DELAY 100000
 #define APPLECOUNT 10
 
 struct applePositions {
@@ -16,10 +16,14 @@ void preventAppleOverlap(int* appleX, int* appleY, int xBound, int yBound) {
     int cycleCount = 1;
     int noOverlap = 0;
     while (!noOverlap) {
+        cycleCount++;
+        if (cycleCount == 10) {
+            break;
+        }
         noOverlap = 1;
         for (int i = 0; i < APPLECOUNT; ++i) {
             for (int j = 0; j < APPLECOUNT; ++j) {
-                if (appleX[i] == appleY[j] && i != j) {
+                if (appleX[i] == appleX[j] &&  appleY[i] == appleY[j] && i != j) {
                     noOverlap = 0;
                     appleX[i] = rand() % xBound;
                     appleY[i] = rand() % yBound;
@@ -39,9 +43,11 @@ int main(void) {
 
     struct applePositions apples;
 
+
+    // TODO add a dynamiclly allocated array for the tail positions
     int snakeTailSize = 1;
-    int snakeTailXPos[1000];
-    int snakeTailYPos[1000];
+    int snakeTailXPositions[1000];
+    int snakeTailYPositions[1000];
 
     initscr();
     noecho();
@@ -49,10 +55,13 @@ int main(void) {
     nodelay(stdscr, TRUE);
     getmaxyx(stdscr, maxY, maxX);
 
+    // Generates the initial positions of the apples
     for (int i = 0; i < APPLECOUNT; i++) {
         apples.xCoordinates[i] = rand() % maxX;
         apples.yCoordinates[i] = rand() % maxY;
     }
+
+    preventAppleOverlap(apples.xCoordinates, apples.yCoordinates, maxX, maxY);
 
     while (TRUE) {
         ch = getch();
@@ -61,11 +70,14 @@ int main(void) {
         char* score = malloc(sizeof(char) * 100);
         sprintf(score, "%i", snakeTailSize - 1);
         mvprintw(0, 0, score);
+        // Renders thee head of the snake at the current position
         mvprintw(y, x, "#");
+        // Renders the tail of the snale at the previous positions that are stored in the array
         for (int i = 1; i < snakeTailSize; ++i) {
-            mvprintw(snakeTailYPos[i], snakeTailXPos[i], "#");
+            mvprintw(snakeTailYPositions[i], snakeTailXPositions[i], "#");
         }
 
+        // Rendering the apples
         for (int i = 0; i < APPLECOUNT; ++i) {
             if (x == apples.xCoordinates[i] & y == apples.yCoordinates[i]) {
                 apples.xCoordinates[i] = -1;
@@ -76,6 +88,7 @@ int main(void) {
         }
         refresh();
         usleep(DELAY);
+        // Input handling 
         for (int i = 0; i < 4; ++i) {
             if (controls[i] == ch) {
                 heading = i;
@@ -99,18 +112,20 @@ int main(void) {
                 break;
         }
 
-        int snakeTailXPosTemp[sizeof(snakeTailXPos)];
-        int snakeTailYPosTemp[sizeof(snakeTailYPos)];
-        memcpy(snakeTailXPosTemp, snakeTailXPos, sizeof(snakeTailXPos));
-        memcpy(snakeTailYPosTemp, snakeTailYPos, sizeof(snakeTailYPos));
-        snakeTailXPos[0] = x;
-        snakeTailYPos[0] = y;
+        // Tail logic
+        int snakeTailXPosTemp[sizeof(snakeTailXPositions)];
+        int snakeTailYPosTemp[sizeof(snakeTailYPositions)];
+        memcpy(snakeTailXPosTemp, snakeTailXPositions, sizeof(snakeTailXPositions));
+        memcpy(snakeTailYPosTemp, snakeTailYPositions, sizeof(snakeTailYPositions));
+        snakeTailXPositions[0] = x;
+        snakeTailYPositions[0] = y;
 
         for (int i = 0; i < snakeTailSize - 1; ++i) {
-            snakeTailXPos[i + 1] = snakeTailXPosTemp[i];
-            snakeTailYPos[i + 1] = snakeTailYPosTemp[i];
+            snakeTailXPositions[i + 1] = snakeTailXPosTemp[i];
+            snakeTailYPositions[i + 1] = snakeTailYPosTemp[i];
         }
 
+        // Respawning already eaten apples
         for (int i = 0; i < APPLECOUNT; ++i) {
             if (apples.xCoordinates[i] == -1) {
                 apples.xCoordinates[i] = rand() % maxX;
@@ -120,6 +135,7 @@ int main(void) {
 
         preventAppleOverlap(apples.xCoordinates, apples.yCoordinates, maxX, maxY);
 
+        // End the game if snake is offscreen
         if (x >= maxX || x < 0 || y >= maxY || y < 0) {
             break;
         }
